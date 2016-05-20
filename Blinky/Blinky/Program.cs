cc7fe67407b6bfd73ba784cdb52e7879f1385474
Richard Wilde <richard@wildesoft.net>
2016-05-20 09:02:26 +0100

    Initial commit

---
+++ Blinky/Blinky/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Raspberry.IO.GeneralPurpose;
+
+namespace Blinky
+{
+    class Program
+    {
+        //private static bool ledIsOn = false;
+
+        static void Main(string[] args)
+        {
+
+
+
+            // Here we create a variable to address a specific pin for output
+            // There are two different ways of numbering pins--the physical numbering, and the CPU number
+            // "P1Pinxx" refers to the physical numbering, and ranges from P1Pin01-P1Pin40
+            var led1 = ConnectorPin.P1Pin07.Output();
+
+            led1.StatusChangedAction += StatusChangedAction;
+
+            // Here we create a connection to the pin we instantiated above
+            var connection = new GpioConnection(led1);
+
+            for (var i = 0; i < 100; i++)
+            {
+                // Toggle() switches the high/low (on/off) status of the pin
+                connection.Toggle(led1);
+                System.Threading.Thread.Sleep(250);
+            }
+
+
+
+
+
+            connection.Close();
+
+        }
+
+        private static void StatusChangedAction(bool state)
+        {
+            Console.WriteLine(state ? "On" : "Off");
+            //ledIsOn = state;
+        }
+    }
+}
---
+++ Thermy/Themy/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Raspberry.IO.Components.Converters.Mcp3008;
+using Raspberry.IO.Components.Devices.PiFaceDigital;
+using Raspberry.IO.Components.Sensors;
+using Raspberry.IO.Components.Sensors.Temperature.Tmp36;
+using Raspberry.IO.GeneralPurpose;
+using UnitsNet;
+
+namespace Themy
+{
+    class Program
+    {
+
+        private static PiFaceDigitalDevice piFace;
+        private static bool polling = true;
+        private static Task t;
+
+        static void Main(string[] args)
+        {
+            piFace = new PiFaceDigitalDevice();
+
+
+            // setup events
+            foreach (var ip in piFace.InputPins)
+            {
+                ip.OnStateChanged += ip_OnStateChanged;
+            }
+
+            t = Task.Factory.StartNew(() => PollInputs());
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int pin = 0; pin < 8; pin++)
+                {
+                    piFace.OutputPins[pin].State = true;
+                    piFace.UpdatePiFaceOutputPins();
+                    Thread.Sleep(500);
+                    piFace.OutputPins[pin].State = false;
+                    piFace.UpdatePiFaceOutputPins();
+                    Thread.Sleep(500);
+                }
+            }
+
+            //stop polling loop
+            polling = false;
+            t.Wait();
+
+
+        }
+
+        /// <summary>
+        /// Loop polling the inputs at 200ms intervals
+        /// </summary>
+        private static void PollInputs()
+        {
+            while (polling)
+            {
+                piFace.PollInputPins();
+                Thread.Sleep(200);
+            }
+        }
+
+        /// <summary>
+        /// Log any input pin changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void ip_OnStateChanged(object sender, InputPinChangedArgs e)
+        {
+            Console.WriteLine("Pin {0} became {1}", e.pin.Id, e.pin.State);
+        }
+    }
+}
